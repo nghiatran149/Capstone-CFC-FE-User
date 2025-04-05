37ebdc17cdce7290c48f302a774769a50cd30b65
nghiatran149 <mrnghia02@gmail.com>
2025-04-05 16:09:07 +0700

    Move ChatBox to Layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -253,7 +253,7 @@ const Homepage = () => {
                 </div>
             </section>
 
-            <ChatBox />
+            {/* <ChatBox /> */}
             <ScrollToTop />
             <Footer />
         </div>
